da732ada4cc7de130ffa85def675fa4c82612be6
Todd <toddtaylor189@gmail.com>
2024-07-22 23:14:47 +0100

    cleaned image and 1 second delay added

--- sketch.js
+++ sketch.js
@@ -4,67 +4,6 @@
 // https://youtu.be/0j86zuqqTlQ
 // https://editor.p5js.org/codingtrain/sketches/S-es-dYVn
 
-// let x;
-// let y;
-
-// let xspeed;
-// let yspeed;
-
-// let dvd;
-
-// let r, g, b;
-
-// function preload() {
-//   dvd = loadImage('whiteLogo.png');
-// }
-
-// const myTimeout = setTimeout(setup, 2000) 
-
-
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-//   x = random(width);
-//   y = random(height);
-//   xspeed = 4;
-//   yspeed = 4;
-//   pickColor();
-// }
-
-// function pickColor() {
-//   r = random(100, 256);
-//   g = random(100, 256);
-//   b = random(100, 256);
-// }
-
-// function draw() {
-//   background(10,1,);
-//   // rect(x, y, 80, 60);
-//   tint(r, g, b);
-//   image(dvd, x, y);
-
-//   x = x + xspeed;
-//   y = y + yspeed;
-
-//   if (x + dvd.width >= width) {
-//     xspeed = -xspeed;
-//     x = width - dvd.width;
-//     pickColor();
-//   } else if (x <= 0) {
-//     xspeed = -xspeed;
-//     x = 0;
-//     pickColor();
-//   }
-
-//   if (y + dvd.height >= height) {
-//     yspeed = -yspeed;
-//     y = height - dvd.height;
-//     pickColor();
-//   } else if (y <= 0) {
-//     yspeed = -yspeed;
-//     y = 0;
-//     pickColor();
-//   }
-// }
 
 let x;
 let y;
@@ -76,27 +15,23 @@ let dvd;
 
 let r, g, b;
 
+let startTime;
+
 function preload() {
-  dvd = loadImage('whiteLogo.png');
+  dvd = loadImage('VEXLOGO.png');
 }
 
-// Initialize the setup after a delay
-const myTimeout = setTimeout(setup, 2000);
-
+// Initialize the setup immediately
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  x = width / 2 - dvd.width / 2;  // Center the image horizontally
-  y = height / 2 - dvd.height / 2; // Center the image vertically
+  x = width / 2 - dvd.width / 2;
+  y = height / 2 - dvd.height / 2;
   xspeed = 4;
   yspeed = 4;
   pickColor();
 
-  // Delay the movement by 1 second
-  setTimeout(startMoving, 1000);
-}
-
-function startMoving() {
-  loop();
+  // Record the start time
+  startTime = millis();
 }
 
 function pickColor() {
@@ -111,8 +46,12 @@ function draw() {
   tint(r, g, b);
   image(dvd, x, y);
 
-  // Move the image only if the draw loop is running
-  if (isLooping()) {
+  // Get the current time
+  let currentTime = millis();
+  
+  // Check if 1 second has passed
+  if (currentTime - startTime > 1000) {
+    // Move the image only if more than 1 second has passed
     x = x + xspeed;
     y = y + yspeed;
 
